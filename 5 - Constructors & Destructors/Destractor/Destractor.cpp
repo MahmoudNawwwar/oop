e9563a97882446d9920a81cd5d9925e9cd9c8753
Mahmoud nawwar <mahmoudnaawwwaar@gmail.com>
2025-06-17 03:38:34 +0300

    add Destractor for  "5 - Constructors & Destructors"

---
+++ 5 - Constructors & Destructors/Destractor/Destractor.cpp
@@ -0,0 +1,52 @@
+﻿// ==============================================================================
+// This program demonstrates the concept of Destructors in C++.
+//
+// - When an object is created, the **constructor** is automatically called.
+// - When an object goes out of scope or is deleted, the **destructor** is called.
+//
+// ▶ In `Fun1()`, the object is created on the stack (local scope), so the destructor
+//   is automatically called after the function ends.
+//
+// ▶ In `Fun2()`, the object is created on the heap using `new`, so we must explicitly
+//   use `delete` to call the destructor and free memory.
+//
+// This demonstrates how destructors are important for memory management in C++.
+// ==============================================================================
+
+#include <iostream>
+using namespace std;
+
+class clsPerson {
+public:
+    string FullName;
+
+    // Constructor
+    clsPerson() {
+        FullName = "Mohammed Abu-Hadhoud";
+        cout << "\nHi, I'm Constructor";
+    }
+
+    // Destructor
+    ~clsPerson() {
+        cout << "\nHi, I'm Destructor";
+    }
+};
+
+void Fun1() {
+    clsPerson Person1;
+    // Destructor will be called automatically when Person1 goes out of scope.
+}
+
+void Fun2() {
+    clsPerson* Person2 = new clsPerson;
+    // Since we used new, we must delete manually to call the destructor.
+    delete Person2;
+}
+
+int main() {
+    Fun1();
+    Fun2();
+
+    system("pause>0");
+    return 0;
+}
