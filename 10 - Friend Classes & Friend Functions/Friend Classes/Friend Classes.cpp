8c6d846eb6753e9dcc35fa77cd8c3be86f1a19fe
Mahmoud nawwar <mahmoudnaawwwaar@gmail.com>
2025-06-18 16:23:55 +0300

    add "Friend Classes"

---
+++ 10 - Friend Classes & Friend Functions/Friend Classes/Friend Classes.cpp
@@ -0,0 +1,53 @@
+// This code demonstrates the concept of friend classes in C++.
+// clsB is declared as a friend of clsA, which allows clsB to access clsA's private and protected members.
+// This is useful when another class needs deep access to internal data of the first class, without exposing that data publicly.
+
+
+#include<iostream>
+using namespace std;
+
+class clsA
+{
+private:
+    int _Var1;
+
+protected:
+    int _Var3;
+
+public:
+    int Var2;
+
+    clsA()
+    {
+        _Var1 = 10;
+        Var2 = 20;
+        _Var3 = 30;
+
+    }
+    //this will grant access for everything to class B 
+    friend class clsB;    //friend class 
+
+};
+
+class clsB
+{
+public:
+    void display(clsA A1)
+    {
+        cout << endl << "The value of Var1=" << A1._Var1;
+        cout << endl << "The value of Var2=" << A1.Var2;
+        cout << endl << "The value of Var3=" << A1._Var3;
+    }
+};
+
+int main()
+{
+    clsA A1;
+    clsB B1;
+
+    B1.display(A1);
+
+    system("pause>0");
+
+    return 0;
+};
